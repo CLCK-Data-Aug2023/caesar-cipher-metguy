6b674f4f767b61147fb2c2e98d7a560071369bbd
Emmett Blair <emmettblair.eb@gmail.com>
2023-11-11 07:26:09 -0500

    Code added for caesar cipher assignment

--- cipher.py
+++ cipher.py
@@ -1,1 +1,39 @@
-# add your code here
+alpha_shift = {
+    "a": "f",
+    "b": "g",
+    "c": "h",
+    "d": "i",
+    "e": "j",
+    "f": "k",
+    "g": "l",
+    "h": "m",
+    "i": "n",
+    "j": "o",
+    "k": "p",
+    "l": "q",
+    "m": "r",
+    "n": "s",
+    "o": "t",
+    "p": "u",
+    "q": "v",
+    "r": "w",
+    "s": "x",
+    "t": "y",
+    "u": "z",
+    "v": "a",
+    "w": "b",
+    "x": "c",
+    "y": "d",
+    "z": "e",
+}
+
+plaintext = str(input("Please type a sentence:")).lower()
+encrypted = ""
+
+for letter in plaintext:
+    if letter in alpha_shift:
+        encrypted += alpha_shift[letter]
+    else:
+        encrypted += letter
+      
+print("Your encrypted text is:", encrypted)
